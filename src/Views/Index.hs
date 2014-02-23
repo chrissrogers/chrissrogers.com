fbc2b0b0ea1f9c03da5214f13a10e18ef8e2ddf4
Christopher Rogers <chrissrogers@gmail.com>
2014-02-23 13:55:29 -0800

    Adds helpers and script.js
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- src/Views/Index.hs
+++ src/Views/Index.hs
@@ -12,11 +12,23 @@ render = docTypeHtml $ do
   html $ do
     H.head $ do
       H.title "chrissrogers.com"
-      link ! rel "stylesheet" ! A.type_ "text/css" ! href "style.css"
+      css "style.css"
+      js "https://cdnjs.cloudflare.com/ajax/libs/three.js/r61/three.min.js"
+      js "script.js"
     body $ do
       section $ do
         header $ do
           img ! src "/icon.png" ! A.title "christopher rogers"
         ul $ do
-          li $ a ! href "https://github.com/chrissrogers" $ "github"
-          li $ a ! href "https://twitter.com/sircrog" $ "twitter"
+          listLink "https://github.com/chrissrogers" "github"
+          listLink "https://twitter.com/sircrog" "twitter"
+
+js :: AttributeValue -> Html
+js loc = H.script ! src loc $ do
+  ""
+
+css :: AttributeValue -> Html
+css loc = link ! rel "stylesheet" ! A.type_ "text/css" ! A.href loc
+
+listLink :: AttributeValue -> Markup -> Html
+listLink loc txt = li $ a ! href loc $ txt
